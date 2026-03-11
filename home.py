1407ecbdf37350a0f66a15d90289f9563280f2cb
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2026-03-11 08:49:07 +0545

    Added data-type of variable

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-#*************** Task-1 *******************
+#*************** Task-1- Aishwarya *******************
 
 name = input("Enter your name: ")
 salary = int(input("Enter your monthly salary: "))
@@ -6,3 +6,8 @@ salary = int(input("Enter your monthly salary: "))
 print("***** Employee Details *****")
 print("Name:", name)
 print("Monthly Salary:", salary)
+
+#*************** Task-2- Anuja *******************
+print(type(salary))
+salary = float(salary)
+print(type(salary))
