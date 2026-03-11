b59ef9193a06af93ae487ec75e4807bc90ea1cc2
anuskabhandari <bhandarirekha652@gmail.com>
2026-03-11 10:34:55 -0300

    Added TASK 3

--- home.py
+++ home.py
@@ -1,1 +1,11 @@
-print('i am anuska helooooooo')
+##task 3 added by anuska ##
+# Input salary
+salary = float(input("Enter your monthly salary: "))
+
+# Calculate tax using if statement
+if salary > 0:
+    tax = salary * 0.10
+    print("Tax amount is:", tax)
+else:
+    tax = 0
+    print("Invalid salary! Tax cannot be calculated.")
