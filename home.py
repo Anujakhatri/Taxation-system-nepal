478a63b1a9f3a28c86b0c61163f67f7fbb3ff96c
anuskabhandari <bhandarirekha652@gmail.com>
2026-03-11 11:24:51 -0300

    Updated one

--- home.py
+++ home.py
@@ -1,6 +1,18 @@
-# Input salary
-salary = float(input("Enter your monthly salary: "))
+#*************** Task-1- Aishwarya *******************
 
+name = input("Enter your name: ")
+salary = int(input("Enter your monthly salary: "))
+
+print("***** Employee Details *****")
+print("Name:", name)
+print("Monthly Salary:", salary)
+
+#*************** Task-2- Anuja *******************
+print(type(salary))
+salary = float(salary)
+print(type(salary))
+
+#******TASK 3 BY ANUSKA******#
 # Calculate tax using if statement
 if salary > 0:
     tax = salary * 0.10
