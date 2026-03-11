7868216a774727b2899384d68c9c02e808b23e8d
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2026-03-11 09:26:07 +0545

    Refactor salary input handling in home.py
    Removed employee name input and print statements. Retained salary input and added type checks.

--- home.py
+++ home.py
@@ -1,13 +1,5 @@
-#*************** Task-1- Aishwarya *******************
-
-name = input("Enter your name: ")
-salary = int(input("Enter your monthly salary: "))
-
-print("***** Employee Details *****")
-print("Name:", name)
-print("Monthly Salary:", salary)
-
 #*************** Task-2- Anuja *******************
+salary = int(input("Enter your monthly salary: "))
 print(type(salary))
 salary = float(salary)
 print(type(salary))
