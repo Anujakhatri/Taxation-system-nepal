722fc4e5ae1bbb7df7889e8918d20a97ab96b0c8
anuskabhandari <bhandarirekha652@gmail.com>
2026-04-09 12:33:14 -0300

    Merged my Django work with latest main

--- django_tax/taxation/models.py
+++ django_tax/taxation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    salary = models.FloatField()
+    allowance = models.FloatField()
+    deductions = models.FloatField()
+    pan_number = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
--- django_tax/taxation/views.py
+++ django_tax/taxation/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from .models import Employee
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-
 def add_employee(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        salary = float(request.POST.get('salary'))
+        allowance = float(request.POST.get('allowance'))
+        deductions = float(request.POST.get('deductions'))
+        pan = request.POST.get('pan')
+
+        Employee.objects.create(
+            name=name,
+            salary=salary,
+            allowance=allowance,
+            deductions=deductions,
+            pan_number=pan
+        )
+
+        messages.success(request, "Employee added successfully!")
+        return redirect('add_employee')
+
     return render(request, 'add_employee.html')
 
 def update_employee(request):
     return render(request, 'update_employee.html')
 
 def view_report(request):
-    return render(request, 'report.html')
+    employees = Employee.objects.all()  # Get all employees from DB
+    return render(request, 'report.html', {'employees': employees})
