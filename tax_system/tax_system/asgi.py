c7ed0699d5e8eebebe01c3b99d196d7f21d660ce
Anuja khatri <khatrianuja20@gmail.com>
2026-04-25 12:54:43 +0545

    refactor: rename project to tax_system and update Nepali language labels in contact page

--- tax_system/tax_system/asgi.py
+++ tax_system/tax_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for कर_सम्रचना project.
+ASGI config for tax_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'कर_सम्रचना.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_system.settings')
 
 application = get_asgi_application()
